67174322f453608e787e78b3567b20de88e366a2
Shayne <shaynefrances.ocay.0405@gmail.com>
2026-07-12 20:44:55 +0800

    changed the name on the navbar to a logo and SIMS text

--- includes/header.php
+++ includes/header.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Healing Paws Inventory</title>
+    <title>Healing Paws Animal Clinic - Inventory</title>
     
     <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -15,7 +16,13 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm mb-4">
     <div class="container-fluid">
-        <a class="navbar-brand fw-bold" href="index.php">🐾 Healing Paws Clinic</a>
+        <a class="navbar-brand fw-bold" href="index.php">
+            <img src="/assets/images/website/HPAC (Logo + Wordmark).png" class="navbar-logo">
+        </a>
+    </div>
+
+    <div class="container-fluid">
+        <a class="navbar-brand fw-bold">Simple Inventory Management System</a>
     </div>
 </nav>
 
