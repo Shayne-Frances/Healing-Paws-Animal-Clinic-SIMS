6a2749629db4901014ecb60ec574e71aa4001888
Shayne <shaynefrances.ocay.0405@gmail.com>
2026-07-12 21:50:02 +0800

    added the retrieve_product logic

---
+++ actions/main_table/retrieve_products.php
@@ -0,0 +1,13 @@
+<?php
+$sql = "SELECT p.*, c.category_name 
+        FROM products p 
+        LEFT JOIN categories c ON p.category_id = c.category_id 
+        ORDER BY p.brand_name ASC";
+
+$products_result = $conn->query($sql);
+
+// If the query fails, we can catch the error
+if (!$products_result) {
+    die("Error retrieving products: " . $conn->error);
+}
+?>
